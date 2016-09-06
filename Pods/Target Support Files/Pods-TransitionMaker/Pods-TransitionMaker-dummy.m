9799c5303bf3ca4fa09726d13364fe7a2e713612
Matan Cohen <matan.cohen@jivesoftware.com>
2016-09-06 17:23:25 +0300

    Releas 0.0.2

---
+++ Pods/Target Support Files/Pods-TransitionMaker/Pods-TransitionMaker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TransitionMaker : NSObject
+@end
+@implementation PodsDummy_Pods_TransitionMaker
+@end
---
+++ Pods/Target Support Files/Pods-TransitionMakerTests/Pods-TransitionMakerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TransitionMakerTests : NSObject
+@end
+@implementation PodsDummy_Pods_TransitionMakerTests
+@end
